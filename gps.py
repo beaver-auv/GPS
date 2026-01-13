36de4ebc02f494c2f74f71d004eeaea4d61c0f06
margokuzn <kuznetsovam27@bcdschool.org>
2026-01-13 14:34:47 -0500

    Update GPS data handling for two serial ports

--- gps.py
+++ gps.py
@@ -1,23 +1,30 @@
 import serial,time,pynmea2
 
-port = '/dev/serial0'
+gps1_port = '/dev/serial0'
+gps2_port = '/dev/serial1'
+
 baud = 9600
-serialPort = serial.Serial(port, baudrate = baud, timeout = 0.5)
+gps1_serialPort = serial.Serial(gps1_port, baudrate = baud, timeout = 0.5)
+gps2_serialPort = serial.Serial(gps2_port, baudrate = baud, timeout = 0.5)
 
 while True:
     str = ''
     try:
-        str = serialPort.readline().decode().strip()
+        gps1_str = gps1_serialPort.readline().decode().strip()
+        gps2_str = gps2_serialPort.readline().decode().strip() #serial port data
     except Exception as e:
         print(e)
-    #print(str)
-        
-    if str.find('GGA') > 0:
-        msg = None
+
+    if 'GGA' in gps1_str or 'GGA' in gps2_str:  #whychange
+        gps1_msg = None
+        gps2_msg = None
         try:
-            msg = pynmea2.parse(str)
-            strMsg = "Timestamp: %s,Lat: %s Lon: %s,Altitude: %s %s,Satellites: %s" % (msg.timestamp,round(msg.latitude,6),round(msg.longitude,6),msg.altitude,msg.altitude_units,msg.num_sats)
-            print(strMsg)             
+            gps1_msg = pynmea2.parse(gps1_str)
+            gps2_msg = pynmea2.parse(gps2_str)
+            gps1_strMsg = "Timestamp: %s,Lat: %s Lon: %s,Altitude: %s %s,Satellites: %s" % (gps1_msg.timestamp,round(gps1_msg.latitude,6),round(gps1_msg.longitude,6),gps1_msg.altitude,gps1_msg.altitude_units,gps1_msg.num_sats)
+            gps2_strMsg = "Timestamp: %s,Lat: %s Lon: %s,Altitude: %s %s,Satellites: %s" % (gps2_msg.timestamp,round(gps2_msg.latitude,6),round(gps2_msg.longitude,6),gps2_msg.altitude,gps2_msg.altitude_units,gps2_msg.num_sats)
+            print(gps1_strMsg)  
+            print(gps2_strMsg)
         except Exception as e:
             print(e)
     time.sleep(0.01)
